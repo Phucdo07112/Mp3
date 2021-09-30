d8e76e9d960e6ab0abd43ada2af6a3535450333f
Phucdo07112 <phucdo0711@gmail.com>
2021-09-30 15:48:00 +0700

    uploat source code

---
+++ js/main.js
@@ -0,0 +1,123 @@
+var modalBtn = document.querySelector('.modal-btn__1');
+var modalBg = document.querySelector('.modal-bg');
+var modalIdol = document.querySelector('.modal-idoi');
+var modalClose = document.querySelector('.modal-close');
+var menuRight = document.querySelector('.menu-right__item-cl');
+var menuDropdown = document.querySelector('.menu-right__drop');
+// music
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title = document.querySelector('#title');
+const cover = document.querySelector('.radio-music__img');
+
+
+
+
+// Song titles
+const songs = ['Là Ai Mang Nắng Đi Xa', 'Phố Không Em', 'Thích Em Hơi Nhiều']
+
+// Keep track of songs
+let songIndex = 2
+
+// Initially load song info DOM
+loadSong(songs[songIndex])
+
+// Update song details
+function loadSong(song) {
+    title.innerText = song
+    audio.scr = `music/${song}.mp3`
+    cover.scr = `images/${song}.jpg`
+}
+
+function playSong(){
+    musicContainer.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play()
+}
+function pauseSong(){
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+    audio.pause()
+}
+
+function prevSong() {
+    songIndex--
+
+    if(songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+function nextSong() {
+    songIndex++
+
+    if(songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+function setProgress(e){
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
+}
+// Event listeners
+play.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play')
+
+    if (isPlaying) {
+        pauseSong()
+    }else{
+        playSong()
+    }
+})
+
+// Chang song events
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
+
+
+
+
+modalBtn.addEventListener('click', function(){
+    modalBg.classList.add('bg-active');
+});
+menuRight.addEventListener('click', function(){
+    menuDropdown.classList.toggle('bg-active');
+});
+modalClose.addEventListener('click', function(){
+    modalBg.classList.remove('bg-active');
+});
+modalBg.addEventListener('click', function(){
+    modalBg.classList.remove('bg-active');
+});
+modalIdol.addEventListener('click', function(event){
+    event.stopPropagation()
+});
+
